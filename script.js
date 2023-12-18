114c15dfc1070c98a21a4b3996b6fd2ae4bb0a18
Alistair McCann <allyjmccann@gmail.com>
2023-12-18 13:58:28 +0100

    pseudokod till uppgift ett

--- script.js
+++ script.js
@@ -2,6 +2,44 @@
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
 
+SET variabel numberOfFriends till användarInput i siffror;
+SET variabel tip till användarInput i procent; (en procentsats 10% = 0.10)
+SET variabel total =till användarInput i siffror;
+
+
+Input "tip"
+    If "tip" är en siffra fortsätt till nästa steg;
+        If tip > 0; (Kollar om siffran är mer än noll)
+        End If    
+    End If
+    Else If "tip" är annat än siffra go tillbaka till input "tip";
+    End Else
+End Input
+
+Input "total"
+    If "total" är en siffra fortsätt till nästa steg;
+       If total > 0; 
+        End If
+    Else If "total" är annat än siffra gå tillbaka till input "total";
+    End Else
+    Calculate
+        totalsumman (total * tips)
+        PRINT totalsumman
+End Input
+
+Input "numberOfFriends"
+    If "numberOfFriends" är en siffra fortsätt till nästa steg;
+        If total > 0; 
+        End If
+    End If
+    Else If "numberOfFriends" är annat än siffra gå tillbaka till input "numberOfFriends";
+    End Else
+End Input
+
+Calculate "pricePerPerson"
+    pricePerPerson === (totalsumman / numberOfFriends)
+    PRINT pricePerPerson
+
 
 */
 
