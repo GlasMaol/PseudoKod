ed8d2d5b4592317f15f28e5605f8e842fb0b26e9
Alistair McCann <allyjmccann@gmail.com>
2023-12-19 09:21:33 +0100

    added calculations plus små ändringar

--- script.js
+++ script.js
@@ -4,44 +4,53 @@
 
 SET variabel numberOfFriends till användarInput i siffror;
 SET variabel tip till användarInput i procent; (en procentsats 10% = 0.10)
-SET variabel total =till användarInput i siffror;
+SET variabel total till användarInput i siffror;
 
 
 Input "tip"
-    If "tip" är en siffra fortsätt till nästa steg;
+    If "tip" är en siffra RETURN Input och fortsätt till nästa steg;
         If tip > 0; (Kollar om siffran är mer än noll)
         End If    
     End If
-    Else If "tip" är annat än siffra go tillbaka till input "tip";
+    Else If "tip" är annat än siffra gå tillbaka till input "tip";
+        PRINT "Oj, verkar som du inte skrev in en giltig siffra!"
     End Else
 End Input
 
 Input "total"
-    If "total" är en siffra fortsätt till nästa steg;
-       If total > 0; 
-        End If
-    Else If "total" är annat än siffra gå tillbaka till input "total";
-    End Else
-    Calculate variabel totalSumman
-        totalSumman === (total * tips)
-        PRINT totalsumman
-End Input
-
-Input "numberOfFriends"
-    If "numberOfFriends" är en siffra fortsätt till nästa steg;
+    If "total" är en siffra RETUEN Input och fortsätt till nästa steg;
         If total > 0; 
         End If
     End If
+    Else If "total" är annat än siffra gå tillbaka till input "total";
+        PRINT "Oj, verkar som du inte skrev in en giltig siffra!"
+    End Else
+End Input
+
+Calculate variabel totalSumman
+        totalSumman === (total + (total * tips))
+        PRINT totalsumman
+
+Input "numberOfFriends"
+    If "numberOfFriends" är en siffra RETURN Input och fortsätt till nästa steg;
+        If numberOfFriends > 0; 
+        End If
+    End If
     Else If "numberOfFriends" är annat än siffra gå tillbaka till input "numberOfFriends";
+        PRINT "Oj, verkar som du inte skrev in en giltig siffra!"
     End Else
 End Input
 
 Calculate variabel pricePerPerson
     pricePerPerson === (totalsumman / numberOfFriends)
-    PRINT "Varje person ska betala:" (pricePerPerson)
-
+    PRINT "Varje person ska betala: (pricePerPerson)" 
 
 */
+
+
+
+
+
 
 // VG-Version - Lewis Carroll Word Puzzle
 // Skriv din pseudokod innanför nedanstående kommentarsblock
